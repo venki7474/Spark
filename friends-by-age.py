f3f827f30eae70f062c8b2ac6f6362a65f167385
venki7474 <venkateshejjagiri@gmail.com>
2017-08-31 01:06:33 +0530

    Finding avg frnds by age

--- friends-by-age.py
+++ friends-by-age.py
@@ -11,7 +11,9 @@ def parseLine(line):
 
 lines = sc.textFile("file:///C:/Users/venka_000/Documents/SparkCourse/fakefriends.csv")
 rdd = lines.map(parseLine)
+# Assigning value 1 for each and every value and adding all the values with the same key
 totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+#finding average of all the values
 averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
 results = averagesByAge.collect()
 for result in results:
